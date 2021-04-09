bf1eddb68af288d6a690761086ae2fc323448a8f
liamcli <liam@determined.ai>
2021-04-09 14:24:58 -0700

    Update horovod sha (#85)

--- dockerfile_scripts/install_nccl_and_horovod.sh
+++ dockerfile_scripts/install_nccl_and_horovod.sh
@@ -8,6 +8,6 @@ git clone https://github.com/determined-ai/nccl.git /tmp/det_nccl
 (cd /tmp/det_nccl && git checkout -q 8768cb3f881ce198825294b62a435e3f3a804baf)
 make -C /tmp/det_nccl -j 4
 ldconfig /usr/local/cuda/targets/x86_64-linux/lib/stubs 
-pip install git+https://github.com/determined-ai/horovod.git@6c222d693f40c9900723e028eabfa66c1c72317f
+pip install git+https://github.com/determined-ai/horovod.git@82b1458d5c6d787180dbca60a5a177b2aabddb59
 ldconfig
 rm -rf /tmp/det_nccl
