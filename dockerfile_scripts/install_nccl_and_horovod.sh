d27b37ee77dd53cc89ab930c88b001d196e27686
Ilia Glazkov <ilia@determined.ai>
2021-05-03 09:59:23 -0700

    don't explicitly install NCCL, simplify dockerfile scripting.

--- dockerfile_scripts/install_nccl_and_horovod.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-DEBIAN_FRONTEND=noninteractive apt-get install -y "libnccl-dev=$NCCL_VERSION" "libnccl2=$NCCL_VERSION" --no-install-recommends
-ldconfig /usr/local/cuda/targets/x86_64-linux/lib/stubs
-pip install "$HOROVOD_PIP"
-ldconfig
