844128fb5f7d624ff50738ca72b065f24a421434
Sean Mackrory <mackrory@determined.ai>
2021-07-07 15:18:40 -0600

    Bump image version and NCCL version

--- dockerfile_scripts/build_nccl.sh
+++ dockerfile_scripts/build_nccl.sh
@@ -5,5 +5,5 @@ set -e
 DEBIAN_FRONTEND=noninteractive apt-get install -y libnccl-dev libnccl2 --no-install-recommends
 
 git clone https://github.com/nvidia/nccl.git /tmp/det_nccl
-(cd /tmp/det_nccl && git checkout v2.9.6-1)
+(cd /tmp/det_nccl && git checkout v2.9.9-1)
 make -C /tmp/det_nccl -j 4
