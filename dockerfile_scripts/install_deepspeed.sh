585b8b1287a73f6d4b72bddf4cf69fc01961579a
liamcli <liam@determined.ai>
2022-03-21 21:31:43 -0700

    fix: add missing deepspeed dependency (#140)

--- dockerfile_scripts/install_deepspeed.sh
+++ dockerfile_scripts/install_deepspeed.sh
@@ -3,4 +3,6 @@
 set -e
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y pdsh libaio-dev
+# Triton is needed to build deepspeed's sparse_attn operation.
+pip install triton==1.0.0
 DS_BUILD_OPS=1 pip install $DEEPSPEED_PIP
