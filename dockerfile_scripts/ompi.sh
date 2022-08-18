8af10d1e749fcdcebea364d05b0b48833c6cd810
Nikhil Nunna <nikhilnunna@c02yn0tbjg5m.its.hpecorp.net>
2022-08-18 10:32:58 -0700

    Adding OFI back

--- dockerfile_scripts/ompi.sh
+++ dockerfile_scripts/ompi.sh
@@ -2,11 +2,13 @@
 
 set -x
 
-# Install the Mellanox OFED stack.  Note that this is dependent on what 
+# Install the Mellanox OFED stack.  Note that this is dependent on what
 # the base OS is (ie, Ubuntu 20.04) so if that changes then this needs updated.
 #MOFED_VER=5.0-2.1.8.0
-MOFED_VER=5.5-1.0.3.2
+#MOFED_VER=5.5-1.0.3.2
+MOFED_VER=5.4-3.4.0.0
 OS_VER=$1
+#OS_VER=ubuntu20.04
 PLATFORM=x86_64
 MOFED_TAR_URL="http://content.mellanox.com/ofed/MLNX_OFED-${MOFED_VER}"
 MOFED_TAR="MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}.tgz"
@@ -23,29 +25,55 @@ mkdir -p ${TMP_INSTALL_DIR}                                          && \
           MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}            \
           MLNX_OFED_LINUX.*.logs                                     && \
    rm -rf ${TMP_INSTALL_DIR}
+OFI=$2
+if [ "$OFI" = "1" ]; then
+  # Install OFI
+  OFI_VER=1.15.1
+  OFI_CONFIG_OPTIONS="--prefix ${OFI_INSTALL_DIR}"
+  OFI_SRC_DIR=/tmp/ofi-src
+  OFI_BASE_URL="https://github.com/ofiwg/libfabric/releases/download"
+  OFI_URL="${OFI_BASE_URL}/v${OFI_VER}/libfabric-${OFI_VER}.tar.bz2"
 
+  mkdir -p ${OFI_SRC_DIR}                              && \
+    cd ${OFI_SRC_DIR}                                  && \
+    wget ${OFI_URL}                                    && \
+    tar -xf libfabric-${OFI_VER}.tar.bz2              && \
+    cd libfabric-${OFI_VER}                            && \
+    ./configure ${OFI_CONFIG_OPTIONS}                  && \
+    make install                                       && \
+    cd /tmp                                            && \
+    rm -rf ${OFI_SRC_DIR}
 
-# Install UCX
-UCX_VER=1.10.1
-UCX_CONFIG_OPTIONS="--prefix ${UCX_INSTALL_DIR} --enable-mt"
-UCX_SRC_DIR=/tmp/ucx-src
-UCX_BASE_URL="https://github.com/openucx/ucx/releases/download"
-UCX_URL="${UCX_BASE_URL}/v${UCX_VER}/ucx-${UCX_VER}.tar.gz"
+  #OMPI CONFIG ARGS FOR OFI
+  OMPI_CONFIG_OPTIONS_VAR="--prefix ${OMPI_INSTALL_DIR} --enable-shared --with-verbs --with-cma --with-pic --enable-mpi-cxx --enable-mpi-thread-multiple --with-pmi --with-pmix=internal --with-platform=contrib/platform/mellanox/optimized --disable-ucx --with-libfabric=/container/ofi"
 
-mkdir -p ${UCX_SRC_DIR}                              && \
-  cd ${UCX_SRC_DIR}                                  && \
-  wget ${UCX_URL}                                    && \
-  tar -xzf ucx-${UCX_VER}.tar.gz                     && \
-  cd ucx-${UCX_VER}                                  && \
-  ./contrib/configure-release ${UCX_CONFIG_OPTIONS}  && \
-  make -j8 install                                   && \
-  cd /tmp                                            && \
-  rm -rf ${UCX_SRC_DIR}
+else
+  # Install UCX
+  UCX_VER=1.10.1
+  UCX_CONFIG_OPTIONS="--prefix ${UCX_INSTALL_DIR} --enable-mt"
+  UCX_SRC_DIR=/tmp/ucx-src
+  UCX_BASE_URL="https://github.com/openucx/ucx/releases/download"
+  UCX_URL="${UCX_BASE_URL}/v${UCX_VER}/ucx-${UCX_VER}.tar.gz"
+
+  mkdir -p ${UCX_SRC_DIR}                              && \
+    cd ${UCX_SRC_DIR}                                  && \
+    wget ${UCX_URL}                                    && \
+    tar -xzf ucx-${UCX_VER}.tar.gz                     && \
+    cd ucx-${UCX_VER}                                  && \
+    ./contrib/configure-release ${UCX_CONFIG_OPTIONS}  && \
+    make -j8 install                                   && \
+    cd /tmp                                            && \
+    rm -rf ${UCX_SRC_DIR}
+
+  #OMPI CONFIG ARGS FOR UCX
+  OMPI_CONFIG_OPTIONS_VAR="--prefix ${OMPI_INSTALL_DIR} --enable-shared --with-verbs --with-cma --with-pic --enable-mpi-cxx --enable-mpi-thread-multiple --with-pmi --with-pmix=internal --with-platform=contrib/platform/mellanox/optimized --with-ucx=/container/ucx"
+
+fi
 
 # Install OMPI
 OMPI_VER=v4.1
 OMPI_VER_NUM=4.1.0
-OMPI_CONFIG_OPTIONS="--prefix ${OMPI_INSTALL_DIR} --enable-shared --with-verbs --with-cma --with-pic --enable-mpi-cxx --enable-mpi-thread-multiple --with-pmi --with-pmix=internal --with-platform=contrib/platform/mellanox/optimized --with-ucx=/container/ucx"
+OMPI_CONFIG_OPTIONS=${OMPI_CONFIG_OPTIONS_VAR}
 OMPI_SRC_DIR=/tmp/openmpi-src
 OMPI_BASE_URL="https://download.open-mpi.org/release/open-mpi"
 OMPI_URL="${OMPI_BASE_URL}/${OMPI_VER}/openmpi-${OMPI_VER_NUM}.tar.gz"
@@ -59,4 +87,3 @@ mkdir -p ${OMPI_SRC_DIR}                        && \
   make                                          && \
   make install                                  && \
   rm -rf ${OMPI_SRC_DIR}
-  
