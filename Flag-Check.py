d87a1199498cde5050558fe98db2a4d9b05a05c2
Nikhil Nunna <nikhilnunna@gmail.com>
2022-08-19 11:22:45 -0700

    Added case where comment is inline with Arg

--- Flag-Check.py
+++ Flag-Check.py
@@ -3,24 +3,27 @@
 import sys
 file1 = open("Makefile", "r")
 #print('Number of arguments:', len(sys.argv), 'arguments.') #debugging
-num_args = len(sys.argv) #getting total args
+num_args = len(sys.argv) # getting total args
 if num_args > 1: # if args greater than just file name
     pos = num_args-1
 
     while pos != 1:
         curr_arg=str(sys.argv[pos]) # Current arg = arg at pos in string format
-        #print('Arg:', pos, ' ', curr_arg) #debugging Arg@pos=...
+        #print('Arg:', pos, ' ', curr_arg) # debugging Arg@pos=...
 
-        if "=" in curr_arg: #if there is an equals in the arg
-            arg_name = curr_arg.split('=')[0] #get left side of equals
+        if "=" in curr_arg: # if there is an equals in the arg
+            arg_name = curr_arg.split('=')[0] # get left side of equals
             final_arg_name = "$(" + arg_name + ")"
             # setting Arg does not exist
             flag = 0
             for line in file1:
                 if final_arg_name in line:
                     if "#" in line:
-                        flag = 0 # Arg does not exist if there is a comment
-                        break
+                        arg_pos=line.find(final_arg_name) # find pos of arg
+                        num_pos=line.find("#") # find pos of comment
+                        if num_pos < arg_pos:
+                            flag = 0 # Arg does not exist if there is a comment
+                            break
                     flag = 1
                     break
             if flag == 0:
