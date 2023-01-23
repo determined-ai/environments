5874bc122d2c672f2065e75ee6d27229551f746f
drh-determined-ai <daniel.robert.hunter@hpe.com>
2023-01-23 16:05:36 +0000

    Remove obsolete patch; don't clone the entire kineto tree

--- dockerfile_scripts/torch-tb-profiler-patch.sh
+++ dockerfile_scripts/torch-tb-profiler-patch.sh
@@ -6,10 +6,8 @@ if [ "$TORCH_PROFILER_GIT" ]; then
   TORCH_PROFILER_DIR=/tmp/kineto
   TORCH_TB_GIT_URL="${TORCH_PROFILER_GIT%@*}"
   TORCH_TB_GIT_VER="${TORCH_PROFILER_GIT#*@}"
-  git clone "$TORCH_TB_GIT_URL" "$TORCH_PROFILER_DIR"
+  git clone --depth 1 --branch "$TORCH_TB_GIT_VER" "$TORCH_TB_GIT_URL" "$TORCH_PROFILER_DIR"
   pushd "$TORCH_PROFILER_DIR"/tb_plugin
-  git checkout "$TORCH_TB_GIT_VER"
-  git apply /tmp/det_dockerfile_scripts/torch_tb_profiler_patch.patch
   pip install .
   popd
 fi
