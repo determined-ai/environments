165bb9230bbccc068be207ea29583de0f5dfd8bf
Chris Rickett <cdrickett@gmail.com>
2023-03-03 14:22:32 -0500

    Subject: Update AWS plugin build, NCCL and GDRcopy
    Description:
    
    This mod changes how NCCL is built to try updating to a more recent
    version. It also tries to enable shared linking for libnccl to enable
    users to override it with one they mount into the container.
    
    This mod also adds libgdrapi* libraries to enable the AWS plugin to
    leverage them. It also pulls the modified AWS plugin code to use.

--- dockerfile_scripts/ompi.sh
+++ dockerfile_scripts/ompi.sh
@@ -109,21 +109,32 @@ if [ "$OFI" = "1" ]; then
   AWS_FILE="${AWS_NAME}-${AWS_VER_NUM}"
   # cuda install dir likely dependent on BaseOS (i.e. ubuntu 20.02)
   # in case this changes in the future
-  CUDA_DIR="/usr/local/cuda/targets/x86_64-linux"
+  cuda_ver_str=`echo $CUDA_VERSION | awk -F "." '{print $1"."$2}'`
+  CUDA_DIR="/usr/local/cuda-$cuda_ver_str/targets/x86_64-linux"
+  GDRCOPY_HOME="/usr"
   AWS_CONFIG_OPTIONS="--prefix ${AWS_PLUGIN_INSTALL_DIR} \
 	  --with-libfabric=${OFI_INSTALL_DIR}            \
 	  --with-nccl=${HOROVOD_NCCL_HOME}               \
 	  --with-mpi=${OMPI_INSTALL_DIR}                 \
+          --with-gdrcopy=${GDRCOPY_HOME}                 \
 	  --with-cuda=${CUDA_DIR} ${WITH_AWS_TRACE}"
   AWS_SRC_DIR=/tmp/aws-ofi-nccl
-  AWS_BASE_URL="https://github.com/aws/aws-ofi-nccl/archive/refs/tags"
-  AWS_URL="${AWS_BASE_URL}/${AWS_VER}.tar.gz"
+#  AWS_BASE_URL="https://github.com/aws/aws-ofi-nccl/archive/refs/tags"
+  #  AWS_URL="${AWS_BASE_URL}/${AWS_VER}.tar.gz"
+  # The INTERNAL_DATASERVER variable must exist in the env
+  INTERNAL_DS="http://set.to.your.server.name"
+  INTERNAL_DS_NCCL_PATH="/set/to/nccl/tarball/path"
+  AWS_BASE_URL="http://${INTERNAL_DS}:${INTERNAL_DS_NCCL_PATH}"
+  AWS_URL="${AWS_BASE_URL}/${AWS_NAME}.tar.gz"
 
   mkdir -p ${AWS_SRC_DIR}                         && \
     cd ${AWS_SRC_DIR}                             && \
-    wget -O "${AWS_FILE}.tar.gz" ${AWS_URL}       && \
-    tar -xzf ${AWS_FILE}.tar.gz                   && \
-    cd ${AWS_FILE}                                && \
+    #    wget -O "${AWS_FILE}.tar.gz" ${AWS_URL}       && \
+    #    tar -xzf ${AWS_FILE}.tar.gz                   && \
+    #    cd ${AWS_FILE}                                && \
+    wget -O ${AWS_NAME}.tar.gz ${AWS_URL}         && \
+    tar -xzf ${AWS_NAME}.tar.gz                   && \
+    cd ${AWS_NAME}                                && \
     ./autogen.sh                                  && \
     ./configure ${AWS_CONFIG_OPTIONS}             && \
     make                                          && \
