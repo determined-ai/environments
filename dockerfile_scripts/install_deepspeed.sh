dc1740c0c04ccdbfa9da90e6f0d863f968dd205e
Taylor Ritenour <taylor.ritenour@hpe.com>
2023-03-27 15:48:50 -0700

    parameterizing

--- dockerfile_scripts/install_deepspeed.sh
+++ dockerfile_scripts/install_deepspeed.sh
@@ -5,5 +5,6 @@ set -e
 DEBIAN_FRONTEND=noninteractive apt-get install -y pdsh libaio-dev
 # Triton is needed to build deepspeed's sparse_attn operation.
 python -m pip install triton==1.0.0
-$DEEPSPEED_OPS=1 python -m pip install $DEEPSPEED_PIP --no-binary deepspeed
+export "${DEEPSPEED_OPS}=1" \
+    && python -m pip install $DEEPSPEED_PIP --no-binary deepspeed
 python -m deepspeed.env_report
