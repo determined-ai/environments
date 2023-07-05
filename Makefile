determined-ai/environments

commit de2a15274688cc6dbd96572eb8a247e9f43d85d4

build: add AWS OFI plugin for NCCL GPU based OFI/libfabric builds (#189)
* PAD-232 Initial AWS OFI Plugin

* Subject: Add OMPI+OFI support to GPU build and enable scraping host fabric

Description:

This mod enables the OMPI+OFI build for GPUs the same as is allowed
for CPUs. Further, it adds an entrypoint for both the tf2-cpu and
tf2-gpu images that can scrape up the host libfabric if
requested. This is intended to replace the libfabric in the container
to enable optimal performance on different architectures (non-IB).

The Horovod build on GPUs was also modified to use MPI for GPU
operations for now until NCCL is supported with libfabric.

Tested the OSU get bw on a libfabric based machine using both the GPU
and CPU images and verified that when scraping up the host libfabric
the performance matches expectations. For example:

user@nid000001:/scratch2/user> env USE_HOST_LIBFABRIC=y srun -n 2 --ntasks-per-node=1 --mpi=pmi2 singularity run --bind /scratch2,/usr:/host/usr /scratch2/user/sif/environments_py-3.8-pytorch-1.11-tf-2.8-cpu-mpi-ofi-0.19.4.sif ./osu-micro-benchmarks-5.6.2/mpi/one-sided/osu_get_bw
1                       0.94
2                       1.84
4                       3.80
8                       7.42
16                     15.22
32                     29.77
64                     61.05
128                   118.42
256                   233.74
512                   450.50
1024                  913.88
2048                 1788.54
4096                 3175.02
8192                 4167.06
16384                5227.06
32768                6122.97
65536               18987.43
131072              20382.48
262144              22252.80
524288              23231.07
1048576             23403.87
2097152             23698.02
4194304             23799.45

The important parts are the setting of the USE_HOST_LIBFABRIC=y to
tell the entry point to look for the host libfabric.so to pull into

Dockerfile-default-gpu
@@ -5,6 +5,22 @@ COPY dockerfile_scripts /tmp/det_dockerfile_scripts
 
 RUN /tmp/det_dockerfile_scripts/install_google_cloud_sdk.sh
 
+ARG WITH_NCCL
+# Make it look for the system nccl, which hopefully is the newer one we built
+# and installed
+ENV HOROVOD_NCCL_HOME="/container/nccl"
+ENV HOROVOD_NCCL_LINK=${WITH_NCCL:+SHARED}
+
+RUN if [ -n "${WITH_NCCL}" ]; then /tmp/det_dockerfile_scripts/build_nccl.sh; fi
+
+#USING OFI
+ARG WITH_OFI
+ARG UCX_INSTALL_DIR=/container/ucx
+ARG OMPI_INSTALL_DIR=/container/ompi
+ARG OFI_INSTALL_DIR=/container/ofi
+ENV NCCL_LIB_DIR=${HOROVOD_NCCL_HOME}/lib
+ENV LD_LIBRARY_PATH=${WITH_OFI:+$NCCL_LIB_DIR:$LD_LIBRARY_PATH}
+
 ARG TENSORFLOW_PIP
 ARG TORCH_PIP
 ARG TORCHVISION_PIP
@@ -29,10 +45,15 @@ ARG APEX_GIT
 ARG APEX_PATCH
 RUN /tmp/det_dockerfile_scripts/install_apex.sh
 
-ARG DET_BUILD_NCCL=1
-RUN if [ -n "${DET_BUILD_NCCL}" ]; then /tmp/det_dockerfile_scripts/build_nccl.sh; fi
-ARG HOROVOD_NCCL_LINK=${DET_BUILD_NCCL:+STATIC}
-ARG HOROVOD_NCCL_HOME=${DET_BUILD_NCCL:+/tmp/det_nccl/build}
+ARG AWS_PLUGIN_INSTALL_DIR=/container/aws
+ARG WITH_AWS_TRACE
+ARG INTERNAL_AWS_DS
+ARG INTERNAL_AWS_PATH
+RUN if [ "$WITH_OFI" = "1" ]; then /tmp/det_dockerfile_scripts/build_aws.sh "$WITH_OFI" "$WITH_AWS_TRACE"; fi
+
+#USING OFI
+ARG AWS_LIB_DIR=${AWS_PLUGIN_INSTALL_DIR}/lib
+ENV LD_LIBRARY_PATH=${WITH_OFI:+$AWS_LIB_DIR:$LD_LIBRARY_PATH}
 
 ARG HOROVOD_WITH_TENSORFLOW
 RUN if [ "$HOROVOD_WITH_TENSORFLOW" ]; then export HOROVOD_WITH_TENSORFLOW=$HOROVOD_WITH_TENSORFLOW; fi
@@ -40,9 +61,10 @@ RUN if [ "$HOROVOD_WITH_TENSORFLOW" ]; then export HOROVOD_WITH_TENSORFLOW=$HORO
 ARG HOROVOD_PIP=""
 ARG HOROVOD_WITH_PYTORCH=1
 ARG HOROVOD_WITHOUT_MXNET=1
-ARG HOROVOD_GPU_OPERATIONS=NCCL
 ARG HOROVOD_WITH_MPI
 ARG HOROVOD_CPU_OPERATIONS
+ARG HOROVOD_GPU_OPERATIONS
+ARG HOROVOD_GPU_ALLREDUCE
 ARG HOROVOD_WITHOUT_MPI
 RUN pip install cmake==3.22.4 protobuf==3.20.3
 RUN if [ -n "${HOROVOD_PIP}" ]; then ldconfig /usr/local/cuda/targets/x86_64-linux/lib/stubs && \

Makefile
@@ -34,9 +34,21 @@ ifeq "$(WITH_MPI)" "1"
 	HOROVOD_WITHOUT_MPI := 0
 	HOROVOD_CPU_OPERATIONS := MPI
 	GPU_SUFFIX := -gpu-mpi
+	WITH_AWS_TRACE := 0
+	NCCL_BUILD_ARG := WITH_NCCL
+        ifeq "$(WITH_NCCL)" "1"
+		NCCL_BUILD_ARG := WITH_NCCL=1
+		HOROVOD_GPU_OPERATIONS := NCCL
+		ifeq "$(WITH_AWS_TRACE)" "1"
+			WITH_AWS_TRACE := 1
+		endif
+        else
+		HOROVOD_GPU_OPERATIONS := MPI
+        endif
 	MPI_BUILD_ARG := WITH_MPI=1
 
 	ifeq "$(WITH_OFI)" "1"
+	        GPU_SUFFIX := -gpu-mpi-ofi
 		CPU_SUFFIX := -cpu-mpi-ofi
 		OFI_BUILD_ARG := WITH_OFI=1
 	else
@@ -47,9 +59,11 @@ else
 	PLATFORMS := $(PLATFORM_LINUX_AMD_64),$(PLATFORM_LINUX_ARM_64)
 	WITH_MPI := 0
 	OFI_BUILD_ARG := WITH_OFI
+	NCCL_BUILD_ARG := WITH_NCCL
 	HOROVOD_WITH_MPI := 0
 	HOROVOD_WITHOUT_MPI := 1
 	HOROVOD_CPU_OPERATIONS := GLOO
+	HOROVOD_GPU_OPERATIONS := NCCL
 	MPI_BUILD_ARG := USE_GLOO=1
 endif
 
@@ -132,7 +146,10 @@ build-gpu-cuda-113-base:
 		--build-arg BASE_IMAGE="nvidia/cuda:11.3.1-cudnn8-devel-$(UBUNTU_VERSION)" \
 		--build-arg PYTHON_VERSION="$(PYTHON_VERSION)" \
 		--build-arg UBUNTU_VERSION="$(UBUNTU_VERSION)" \
+		--build-arg WITH_AWS_TRACE="$(WITH_AWS_TRACE)" \
 		--build-arg "$(MPI_BUILD_ARG)" \
+		--build-arg "$(OFI_BUILD_ARG)" \
+		--build-arg "$(NCCL_BUILD_ARG)" \
 		-t $(DOCKERHUB_REGISTRY)/$(GPU_CUDA_113_BASE_NAME)-$(SHORT_GIT_HASH) \
 		-t $(DOCKERHUB_REGISTRY)/$(GPU_CUDA_113_BASE_NAME)-$(VERSION) \
 		.
@@ -204,7 +221,7 @@ build-deepspeed-gpu: build-gpu-cuda-113-base
 		--build-arg TORCH_TB_PROFILER_PIP="$(TORCH_TB_PROFILER_PIP)" \
 		--build-arg TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.5;8.0" \
 		--build-arg APEX_GIT="https://github.com/determined-ai/apex.git@3caf0f40c92e92b40051d3afff8568a24b8be28d" \
-		--build-arg DET_BUILD_NCCL="" \
+		--build-arg "$(NCCL_BUILD_ARG)" \
 		--build-arg DEEPSPEED_PIP="deepspeed==$(DEEPSPEED_VERSION)" \
 		-t $(DOCKERHUB_REGISTRY)/$(GPU_DEEPSPEED_ENVIRONMENT_NAME)-$(SHORT_GIT_HASH) \
 		-t $(DOCKERHUB_REGISTRY)/$(GPU_DEEPSPEED_ENVIRONMENT_NAME)-$(VERSION) \
@@ -222,7 +239,7 @@ build-gpt-neox-deepspeed-gpu: build-gpu-cuda-113-base
 		--build-arg TORCH_TB_PROFILER_PIP="$(TORCH_TB_PROFILER_PIP)" \
 		--build-arg TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.5;8.0" \
 		--build-arg APEX_GIT="https://github.com/determined-ai/apex.git@3caf0f40c92e92b40051d3afff8568a24b8be28d" \
-		--build-arg DET_BUILD_NCCL="" \
+		--build-arg "$(NCCL_BUILD_ARG)" \
 		--build-arg DEEPSPEED_PIP="git+https://github.com/determined-ai/deepspeed.git@eleuther_dai" \
 		-t $(DOCKERHUB_REGISTRY)/$(GPU_GPT_NEOX_DEEPSPEED_ENVIRONMENT_NAME)-$(SHORT_GIT_HASH) \
 		-t $(DOCKERHUB_REGISTRY)/$(GPU_GPT_NEOX_DEEPSPEED_ENVIRONMENT_NAME)-$(VERSION) \
@@ -353,10 +370,16 @@ build-tf2-gpu: build-gpu-cuda-113-base
 		--build-arg TORCH_CUDA_ARCH_LIST="3.7;6.0;6.1;6.2;7.0;7.5;8.0" \
 		--build-arg APEX_GIT="https://github.com/determined-ai/apex.git@3caf0f40c92e92b40051d3afff8568a24b8be28d" \
 		--build-arg HOROVOD_PIP="horovod==0.27.0" \
-		--build-arg DET_BUILD_NCCL="" \
+		--build-arg WITH_AWS_TRACE="$(WITH_AWS_TRACE)" \
+		--build-arg INTERNAL_AWS_DS="$(INTERNAL_AWS_DS)" \
+		--build-arg INTERNAL_AWS_PATH="$(INTERNAL_AWS_PATH)" \
+		--build-arg "$(OFI_BUILD_ARG)" \
+		--build-arg "$(NCCL_BUILD_ARG)" \
 		--build-arg HOROVOD_WITH_MPI="$(HOROVOD_WITH_MPI)" \
 		--build-arg HOROVOD_WITHOUT_MPI="$(HOROVOD_WITHOUT_MPI)" \
 		--build-arg HOROVOD_CPU_OPERATIONS="$(HOROVOD_CPU_OPERATIONS)" \
+		--build-arg HOROVOD_GPU_OPERATIONS="$(HOROVOD_GPU_OPERATIONS)" \
+		--build-arg HOROVOD_GPU_ALLREDUCE="$(HOROVOD_GPU_ALLREDUCE)" \
 		-t $(DOCKERHUB_REGISTRY)/$(GPU_TF2_ENVIRONMENT_NAME)-$(SHORT_GIT_HASH) \
 		-t $(DOCKERHUB_REGISTRY)/$(GPU_TF2_ENVIRONMENT_NAME)-$(VERSION) \
 		-t $(NGC_REGISTRY)/$(GPU_TF2_ENVIRONMENT_NAME)-$(SHORT_GIT_HASH) \
@@ -372,7 +395,7 @@ build-pt-gpu: build-gpu-cuda-113-base
 		--build-arg TORCH_CUDA_ARCH_LIST="3.7;6.0;6.1;6.2;7.0;7.5;8.0" \
 		--build-arg APEX_GIT="https://github.com/determined-ai/apex.git@3caf0f40c92e92b40051d3afff8568a24b8be28d" \
 		--build-arg HOROVOD_PIP="horovod==0.24.2" \
-		--build-arg DET_BUILD_NCCL="" \
+		--build-arg "$(NCCL_BUILD_ARG)" \
 		--build-arg HOROVOD_WITH_MPI="$(HOROVOD_WITH_MPI)" \
 		--build-arg HOROVOD_WITHOUT_MPI="$(HOROVOD_WITHOUT_MPI)" \
 		--build-arg HOROVOD_CPU_OPERATIONS="$(HOROVOD_CPU_OPERATIONS)" \

Dockerfile-base-gpu
@@ -46,25 +46,50 @@ RUN apt-get update \
 
 COPY dockerfile_scripts /tmp/det_dockerfile_scripts
 
+ARG WITH_NCCL
+# Install debuild util, etc. for later compiling GDRcopy libraries
+RUN if [ "$WITH_NCCL" = "1" ]; then apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y devscripts debhelper; fi
+
+# Install GDRcopy so OMPI, NCCL, AWS plugin, etc, can see it
+RUN if [ "$WITH_NCCL" = "1" ]; then /tmp/det_dockerfile_scripts/build_gdrcopy.sh; fi
+
 ENV PATH="/opt/conda/bin:${PATH}"
+ARG CONDA="${PATH}"
+
 ENV PYTHONUNBUFFERED=1 PYTHONFAULTHANDLER=1 PYTHONHASHSEED=0
 ARG PYTHON_VERSION
 RUN /tmp/det_dockerfile_scripts/install_python.sh ${PYTHON_VERSION}
 
+# Install fixed version of FFI package for Ubuntu 20.04.
+# This is done after above stuff to make sure we get right version.
+RUN /tmp/det_dockerfile_scripts/install_package_fixes.sh
+
 ARG WITH_MPI
+ARG WITH_OFI
 ARG UCX_INSTALL_DIR=/container/ucx
 ARG OMPI_INSTALL_DIR=/container/ompi
-RUN if [ "$WITH_MPI" = "1" ]; then /tmp/det_dockerfile_scripts/ompi.sh "$UBUNTU_VERSION" ; fi
+ARG OFI_INSTALL_DIR=/container/ofi
+ARG OMPI_WITH_CUDA=1
+RUN if [ "$WITH_MPI" = "1" ]; then /tmp/det_dockerfile_scripts/ompi.sh "$UBUNTU_VERSION" "$WITH_OFI" "$OMPI_WITH_CUDA"; fi
 # Make sure OMPI/UCX show up in the right paths
 ARG VERBS_LIB_DIR=/usr/lib/libibverbs
 ARG UCX_LIB_DIR=${UCX_INSTALL_DIR}/lib:${UCX_INSTALL_DIR}/lib64
 ARG UCX_PATH_DIR=${UCX_INSTALL_DIR}/bin
+ARG OFI_LIB_DIR=${OFI_INSTALL_DIR}/lib:${OFI_INSTALL_DIR}/lib64
+ARG OFI_PATH_DIR=${OFI_INSTALL_DIR}/bin
 ARG OMPI_LIB_DIR=${OMPI_INSTALL_DIR}/lib
 ARG OMPI_PATH_DIR=${OMPI_INSTALL_DIR}/bin
-ENV LD_LIBRARY_PATH $LD_LIBRARY_PATH:${WITH_MPI:+$VERBS_LIB_DIR:$UCX_LIB_DIR:$OMPI_LIB_DIR}
-ENV PATH $PATH:${WITH_MPI:+$UCX_PATH_DIR:$OMPI_PATH_DIR}
+
+#USING OFI
+ENV LD_LIBRARY_PATH=${WITH_OFI:+$LD_LIBRARY_PATH:${WITH_MPI:+$VERBS_LIB_DIR:$OFI_LIB_DIR:$OMPI_LIB_DIR}}
+ENV PATH=${WITH_OFI:+$PATH:${WITH_MPI:+$OFI_PATH_DIR:$OMPI_PATH_DIR}}
+
+#USING UCX
+ENV LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-$LD_LIBRARY_PATH:${WITH_MPI:+$VERBS_LIB_DIR:$UCX_LIB_DIR:$OMPI_LIB_DIR}}
+ENV PATH=${PATH:-$CONDA:${WITH_MPI:+$UCX_PATH_DIR:$OMPI_PATH_DIR}}
+
 # Enable running OMPI as root
-ENV OMPI_ALLOW_RUN_AS_ROOT ${WITH_MPI:+1} 
+ENV OMPI_ALLOW_RUN_AS_ROOT ${WITH_MPI:+1}
 ENV OMPI_ALLOW_RUN_AS_ROOT_CONFIRM ${WITH_MPI:+1}
 
 
@@ -83,4 +108,12 @@ RUN /tmp/det_dockerfile_scripts/add_det_nobody_user.sh
 
 RUN /tmp/det_dockerfile_scripts/install_libnss_determined.sh
 
+# Set an entrypoint that can scrape up the host libfabric.so and then 
+# run the user command. This is intended to enable performant execution
+# on non-IB systems that have a proprietary libfabric.
+RUN mkdir -p /container/bin && \
+    cp /tmp/det_dockerfile_scripts/scrape_libs.sh /container/bin
+ENTRYPOINT ["/container/bin/scrape_libs.sh"]
+
 RUN rm -r /tmp/*
+

dockerfile_scripts/build_aws.sh
@@ -1,0 +1,67 @@
+#!/bin/bash
+
+set -x
+
+WITH_AWS_TRACE=""
+if [ $# -gt 1 ] ; then
+    if [ "$2" = "1" ] ; then
+	# Tell AWS to build with trace messages enabled
+	WITH_AWS_TRACE="--enable-trace"
+    fi
+fi
+OFI=$1
+
+if [ "$OFI" = "1" ]; then
+  apt-get update \
+        && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+                tcsh
+
+  # Install AWS_OFI_NCCL
+  AWS_VER=v1.4.0
+  AWS_VER_NUM=1.4.0
+  AWS_NAME=aws-ofi-nccl
+  AWS_FILE="${AWS_NAME}-${AWS_VER_NUM}"
+  # cuda install dir likely dependent on BaseOS (i.e. ubuntu 20.02)
+  # in case this changes in the future
+  cuda_ver_str=`echo $CUDA_VERSION | awk -F "." '{print $1"."$2}'`
+  CUDA_DIR="/usr/local/cuda-$cuda_ver_str/targets/x86_64-linux"
+  GDRCOPY_HOME="/usr"
+
+  AWS_CONFIG_OPTIONS="--prefix ${AWS_PLUGIN_INSTALL_DIR} \
+	  --with-libfabric=${OFI_INSTALL_DIR}            \
+	  --with-nccl=${HOROVOD_NCCL_HOME}               \
+	  --with-mpi=${OMPI_INSTALL_DIR}                 \
+	  --with-gdrcopy=${GDRCOPY_HOME}                 \
+	  --with-cuda=${CUDA_DIR} ${WITH_AWS_TRACE}"
+  AWS_SRC_DIR=/tmp/aws-ofi-nccl
+  AWS_BASE_URL="https://github.com/aws/aws-ofi-nccl/archive/refs/tags"
+  AWS_URL="${AWS_BASE_URL}/${AWS_VER}.tar.gz"
+  # The INTERNAL_AWS_DS variable must exist in the env
+  ## INTERNAL_AWS_DS="http://set.to.your.server.name"
+  ## INTERNAL_AWS_PATH="/set/to/aws/tarball/path"
+  if [ -z "$INTERNAL_AWS_DS" ]
+  then
+    echo "Using EXTERNAL AWS $AWS_URL" 
+    # aws-ofi-nccl-1.4.0
+    AWS_NAME="${AWS_NAME}-${AWS_VER_NUM}"
+  else
+    AWS_BASE_URL="http://${INTERNAL_AWS_DS}${INTERNAL_AWS_PATH}"
+    AWS_URL="${AWS_BASE_URL}/${AWS_NAME}.tar.gz"
+    echo "Using INTERNAL AWS $AWS_URL" 
+  fi
+
+  mkdir -p ${AWS_SRC_DIR}                         && \
+    cd ${AWS_SRC_DIR}                             && \
+    #    wget -O "${AWS_FILE}.tar.gz" ${AWS_URL}       && \
+    #    tar -xzf ${AWS_FILE}.tar.gz                   && \
+    #    cd ${AWS_FILE}                                && \
+    wget -O ${AWS_NAME}.tar.gz ${AWS_URL}         && \
+    tar -xzf ${AWS_NAME}.tar.gz                   && \
+    cd ${AWS_NAME}                                && \
+    ./autogen.sh                                  && \
+    ./configure ${AWS_CONFIG_OPTIONS}             && \
+    make                                          && \
+    make install                                  && \
+    cd /tmp                                       && \
+    rm -rf ${AWS_SRC_DIR}
+fi

dockerfile_scripts/build_gdrcopy.sh
@@ -1,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -x
+
+GDR_VER=2.3
+GDR_URL="https://github.com/NVIDIA/gdrcopy/archive/refs/tags"
+GDR_TARBALL="v$GDR_VER.tar.gz"
+cuda_ver_str=`echo $CUDA_VERSION | awk -F "." '{print $1"."$2}'`
+echo "cuda_ver_str: $cuda_ver_str"
+# Clone the GDRCopy github and compile it to build the deb packages. Note
+# that we only build the libgdrapi* stuff and don't do the kernel mods, etc
+# since we're running in a container. This is done just to satisfy linker
+# requirements for some libraries (e.g., libfabric) and the AWS plugin
+cd /tmp   && \
+    wget $GDR_URL/$GDR_TARBALL && \
+    tar zxf $GDR_TARBALL && \
+    cd gdrcopy-$GDR_VER  && \
+    CUDA=/usr/local/cuda-$cuda_ver_str/targets/x86_64-linux \
+       ./packages/build-deb-packages.sh -t -k -d && \
+    cd / && \
+    dpkg -i \
+        /tmp/gdrcopy-$GDR_VER/libgdrapi_$GDR_VER-1_amd64.Ubuntu20_04.deb && \
+    rm -rf /tmp/gdrcopy-$GDR_VER /tmp/$GDR_TARBALL
+

dockerfile_scripts/build_nccl.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y libnccl-dev libnccl2 --no-install-recommends
+#DEBIAN_FRONTEND=noninteractive apt-get install -y libnccl-dev libnccl2 --no-install-recommends
+
+export NVCC_GENCODE="-gencode=arch=compute_80,code=sm_80"
 
 git clone https://github.com/nvidia/nccl.git /tmp/det_nccl
-(cd /tmp/det_nccl && git checkout v2.11.4-1)
-make -C /tmp/det_nccl -j 4
+
+(cd /tmp/det_nccl && git checkout v2.15.5-1)
+
+make PREFIX=${HOROVOD_NCCL_HOME} -C /tmp/det_nccl -j 4 install
+

dockerfile_scripts/install_package_fixes.sh
@@ -1,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+
+PKG_URL="http://archive.ubuntu.com/ubuntu/pool/main/libf/libffi"
+FIXED_FFI_PKG="libffi7_3.3-4_amd64.deb"
+
+# Install fixed version of libffi
+mkdir -p /tmp/ffi && \
+    cd /tmp/ffi && \
+    wget "$PKG_URL/$FIXED_FFI_PKG" && \
+    cd / && \
+    dpkg -i /tmp/ffi/$FIXED_FFI_PKG && \
+    rm -rf /tmp/ffi

dockerfile_scripts/ompi.sh
@@ -2,29 +2,16 @@
 
 set -x
 
-# Install the Mellanox OFED stack.  Note that this is dependent on what
-# the base OS is (ie, Ubuntu 20.04) so if that changes then this needs updated.
-#MOFED_VER=5.0-2.1.8.0
-#MOFED_VER=5.5-1.0.3.2
-MOFED_VER=5.4-3.4.0.0
-OS_VER=$1
-#OS_VER=ubuntu20.04
-PLATFORM=x86_64
-MOFED_TAR_URL="http://content.mellanox.com/ofed/MLNX_OFED-${MOFED_VER}"
-MOFED_TAR="MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}.tgz"
-TMP_INSTALL_DIR=/tmp/ofed
+# See if we should add CUDA to the OMPI build
+OMPI_WITH_CUDA=""
+if [ $# -gt 2 ] ; then
+    if [ "$3" = "1" ] ; then
+	# Tell OMPI to look for cuda in the default location
+	OMPI_WITH_CUDA="--with-cuda"
+    fi
+fi
 
-mkdir -p ${TMP_INSTALL_DIR}                                          && \
-   cd ${TMP_INSTALL_DIR}                                             && \
-   wget --quiet "${MOFED_TAR_URL}/${MOFED_TAR}"                      && \
-   tar -xvf ${MOFED_TAR}                                             && \
-   MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}/mlnxofedinstall   \
-     --user-space-only --without-fw-update --all --force                \
-     --skip-unsupported-devices-check                                && \
-   rm -rf MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}.tgz        \
-          MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}            \
-          MLNX_OFED_LINUX.*.logs                                     && \
-   rm -rf ${TMP_INSTALL_DIR}
+OS_VER=$1
 OFI=$2
 if [ "$OFI" = "1" ]; then
   # Install OFI
@@ -45,9 +32,29 @@ if [ "$OFI" = "1" ]; then
     rm -rf ${OFI_SRC_DIR}
 
   #OMPI CONFIG ARGS FOR OFI
-  OMPI_CONFIG_OPTIONS_VAR="--prefix ${OMPI_INSTALL_DIR} --enable-shared --with-verbs --with-cma --with-pic --enable-mpi-cxx --enable-mpi-thread-multiple --with-pmi --with-pmix=internal --with-platform=contrib/platform/mellanox/optimized --disable-ucx --with-libfabric=/container/ofi"
-
+  OMPI_CONFIG_OPTIONS_VAR="--prefix ${OMPI_INSTALL_DIR} --enable-orterun-prefix-by-default --enable-shared --with-cma --with-pic --enable-mpi-cxx --enable-mpi-thread-multiple --with-libfabric=${OFI_INSTALL_DIR} --without-ucx --with-pmi --with-pmix=internal ${OMPI_WITH_CUDA}"
 else
+  # Install the Mellanox OFED stack.  Note that this is dependent on
+  # what the base OS is (ie, Ubuntu 20.04) so if that changes then
+  # this needs updated.  MOFED_VER=5.0-2.1.8.0 MOFED_VER=5.5-1.0.3.2
+  MOFED_VER=5.4-3.4.0.0
+  PLATFORM=x86_64
+  MOFED_TAR_URL="http://content.mellanox.com/ofed/MLNX_OFED-${MOFED_VER}"
+  MOFED_TAR="MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}.tgz"
+  TMP_INSTALL_DIR=/tmp/ofed
+  
+  mkdir -p ${TMP_INSTALL_DIR}                                          && \
+     cd ${TMP_INSTALL_DIR}                                             && \
+     wget --quiet "${MOFED_TAR_URL}/${MOFED_TAR}"                      && \
+     tar -xvf ${MOFED_TAR}                                             && \
+     MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}/mlnxofedinstall   \
+       --user-space-only --without-fw-update --all --force                \
+       --skip-unsupported-devices-check                                && \
+     rm -rf MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}.tgz        \
+            MLNX_OFED_LINUX-${MOFED_VER}-${OS_VER}-${PLATFORM}            \
+            MLNX_OFED_LINUX.*.logs                                     && \
+     rm -rf ${TMP_INSTALL_DIR}
+
   # Install UCX
   UCX_VER=1.10.1
   UCX_CONFIG_OPTIONS="--prefix ${UCX_INSTALL_DIR} --enable-mt"
@@ -66,7 +73,7 @@ else
     rm -rf ${UCX_SRC_DIR}
 
   #OMPI CONFIG ARGS FOR UCX
-  OMPI_CONFIG_OPTIONS_VAR="--prefix ${OMPI_INSTALL_DIR} --enable-shared --with-verbs --with-cma --with-pic --enable-mpi-cxx --enable-mpi-thread-multiple --with-pmi --with-pmix=internal --with-platform=contrib/platform/mellanox/optimized --with-ucx=/container/ucx"
+  OMPI_CONFIG_OPTIONS_VAR="--prefix ${OMPI_INSTALL_DIR} --enable-shared --with-verbs --with-cma --with-pic --enable-mpi-cxx --enable-mpi-thread-multiple --with-pmi --with-pmix=internal --with-platform=contrib/platform/mellanox/optimized --with-ucx=/container/ucx ${OMPI_WITH_CUDA}"
 
 fi
 
@@ -86,4 +93,5 @@ mkdir -p ${OMPI_SRC_DIR}                        && \
   ./configure ${OMPI_CONFIG_OPTIONS}            && \
   make                                          && \
   make install                                  && \
+  cd /tmp                                       && \
   rm -rf ${OMPI_SRC_DIR}

commit 5cc0e29284dbd38c61771290e91c30504819f6d8

Fix: MPI test failure with images built with WITH_MPI=1
Subject: Fix MPI test failure with images built with WITH_MPI=1

Description:
This mod addresses MPI test failures introduced with commit de2a152.

Updated environment variable NCCL_HOME with the "WITH_NCCL" setting. Updated HOROVOD_GPU_OPERATION to "NCCL" in MPI builds. Builds with "WITH_MPI=0" are unchanged.

Builds are successfully executed on the AWS ubuntu servers.
Tests are executed and passed on Grenoble champollion and slingshot nodes. Tests are executed and passed on Osprey GPU and CPU nodes.

MPI builds:
make DOCKER_BUILD_OPTS="--no-cache" WITH_MPI=1 WITH_NCCL=0 WITH_OFI=0 build-tf1-gpu make DOCKER_BUILD_OPTS="--no-cache" WITH_MPI=1 WITH_NCCL=0 WITH_OFI=0 build-tf2-gpu make DOCKER_BUILD_OPTS="--no-cache" WITH_MPI=1 WITH_NCCL=0 WITH_OFI=0 build-tf1-cpu make DOCKER_BUILD_OPTS="--no-cache" WITH_MPI=1 WITH_NCCL=0 WITH_OFI=0 build-tf2-cpu

MPI with OFI build:
make DOCKER_BUILD_OPTS="--no-cache" WITH_MPI=1 WITH_NCCL=1 WITH_OFI=1 build-tf2-gpu

MPI tests on Grenoble:
env NCCL_DEBUG=TRACE env CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 srun --mpi=pmi2 -n 2 --ntasks-per-node=1 -p champollion singularity exec --nv --bind /cstor /cstor/builder/detai-env-images/detai-cuda-11.3-pytorch-1.12-tf-2.11-gpu-mpi-9d14054.sif /cstor/builder/o184i023/cactus-test-framework/apps/osu-micro-benchmarks-5.7/mpi/one-sided/osu_get_bw

env NCCL_DEBUG=TRACE env CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 srun --mpi=pmi2 -n 2 --ntasks-per-node=4 -p champollion -t 240 singularity exec --nv --bind /cstor /cstor/builder/detai-env-images/detai-cuda-11.3-pytorch-1.12-tf-2.11-gpu-mpi-9d14054.sif python3 test/scripts/tensorflow2_mnist.py

Dockerfile-default-gpu
@@ -8,7 +8,8 @@ RUN /tmp/det_dockerfile_scripts/install_google_cloud_sdk.sh
 ARG WITH_NCCL
 # Make it look for the system nccl, which hopefully is the newer one we built
 # and installed
-ENV HOROVOD_NCCL_HOME="/container/nccl"
+
+ENV HOROVOD_NCCL_HOME=${WITH_NCCL:+"/container/nccl"}
 ENV HOROVOD_NCCL_LINK=${WITH_NCCL:+SHARED}
 
 RUN if [ -n "${WITH_NCCL}" ]; then /tmp/det_dockerfile_scripts/build_nccl.sh; fi

Makefile
@@ -26,6 +26,7 @@ UBUNTU_IMAGE_TAG := ubuntu:20.04
 UBUNTU_VERSION_1804 := ubuntu18.04
 PLATFORM_LINUX_ARM_64 := linux/arm64
 PLATFORM_LINUX_AMD_64 := linux/amd64
+HOROVOD_GPU_OPERATIONS := NCCL 
 
 ifeq "$(WITH_MPI)" "1"
 # 	Don't bother supporting or building arm64+mpi builds.
@@ -38,12 +39,9 @@ ifeq "$(WITH_MPI)" "1"
 	NCCL_BUILD_ARG := WITH_NCCL
         ifeq "$(WITH_NCCL)" "1"
 		NCCL_BUILD_ARG := WITH_NCCL=1
-		HOROVOD_GPU_OPERATIONS := NCCL
 		ifeq "$(WITH_AWS_TRACE)" "1"
 			WITH_AWS_TRACE := 1
 		endif
-        else
-		HOROVOD_GPU_OPERATIONS := MPI
         endif
 	MPI_BUILD_ARG := WITH_MPI=1
 
@@ -63,7 +61,6 @@ else
 	HOROVOD_WITH_MPI := 0
 	HOROVOD_WITHOUT_MPI := 1
 	HOROVOD_CPU_OPERATIONS := GLOO
-	HOROVOD_GPU_OPERATIONS := NCCL
 	MPI_BUILD_ARG := USE_GLOO=1
 endif
 