810d561daaf169932a69817830876f483446346e
Michael Kardash <mikhail.kardash@hpe.com>
2023-07-28 09:36:47 -0700

    Remove tf1 and bugfixes (#221)
    * feat: remove tf1 images and update tf2.8
    
    * switch conda-forge to miniconda installer

--- dockerfile_scripts/install_python.sh
+++ dockerfile_scripts/install_python.sh
@@ -8,13 +8,13 @@ ARCHITECTURE=${2:-linux/amd64}
 CONDA_DIR="/opt/conda"
 
 if [[ "$ARCHITECTURE" == "linux/amd64" ]]; then
-  CONDA_INSTALLER="Miniconda3-py39_4.10.3-Linux-x86_64.sh"
-  CONDA_MD5="8c69f65a4ae27fb41df0fe552b4a8a3b"
+  CONDA_INSTALLER="Miniconda3-py39_23.5.2-0-Linux-x86_64.sh"
+  CONDA_SHA256="9829d95f639bd0053b2ed06d1204e60644617bf37dd5cc57523732e0e8d64516"
   CONDA_URL="https://repo.anaconda.com/miniconda"
 elif [[ "$ARCHITECTURE" == "linux/arm64" ]]; then
-  CONDA_INSTALLER="Miniforge3-4.10.3-10-Linux-aarch64.sh"
-  CONDA_MD5="85d7ea630bb91259bf09f6d52a5ec1c4"
-  CONDA_URL="https://github.com/conda-forge/miniforge/releases/download/4.10.3-10"
+  CONDA_INSTALLER="Miniconda3-py39_23.5.2-0-Linux-aarch64.sh"
+  CONDA_SHA256="ecc06a39bdf786ebb8325a2754690a808f873154719c97d10087ef0883b69e84"
+  CONDA_URL="https://repo.anaconda.com/miniconda"
 else
   echo "Unsupported architecture $ARCHITECTURE"
 fi
@@ -24,7 +24,7 @@ mkdir -p "${CONDA_DIR}"
 
 cd /tmp
 curl --retry 3 -fsSL -O "${CONDA_URL}/${CONDA_INSTALLER}"
-echo "${CONDA_MD5}  ${CONDA_INSTALLER}" | md5sum -c -
+echo "${CONDA_SHA256}  ${CONDA_INSTALLER}" | sha256sum ${CONDA_INSTALLER}
 bash "./${CONDA_INSTALLER}" -u -b -p "${CONDA_DIR}"
 rm -f "./${CONDA_INSTALLER}"
 
