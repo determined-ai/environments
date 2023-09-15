9c179d8f9063e4d2400204d96b706c8674b4430c
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-09-15 14:01:35 -0700

    downgrade torch in deepspeed install

--- dockerfile_scripts/install_deepspeed.sh
+++ dockerfile_scripts/install_deepspeed.sh
@@ -7,5 +7,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y pdsh libaio-dev
 # Triton is needed to build deepspeed's sparse_attn operation.
 python -m pip install triton
 python -m pip install pydantic==1.10.11
+python -m pip install torch==1.13.1
 DS_BUILD_OPS=1 python -m pip install $DEEPSPEED_PIP --no-binary deepspeed
 python -m deepspeed.env_report
