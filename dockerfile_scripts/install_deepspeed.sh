7fe49092beff337c315d9b83a1078d584dc924fc
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-09-15 13:54:11 -0700

    triton version pin removal

--- dockerfile_scripts/install_deepspeed.sh
+++ dockerfile_scripts/install_deepspeed.sh
@@ -5,7 +5,7 @@ set -e
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y pdsh libaio-dev
 # Triton is needed to build deepspeed's sparse_attn operation.
-python -m pip install triton==1.0.0
+python -m pip install triton
 python -m pip install pydantic==1.10.11
 DS_BUILD_OPS=1 python -m pip install $DEEPSPEED_PIP --no-binary deepspeed
 python -m deepspeed.env_report
