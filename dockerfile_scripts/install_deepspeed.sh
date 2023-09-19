5460b14fc1975777f0ce910f569c86ac4c0bde69
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-09-19 15:59:38 -0700

    set deepspeed version

--- dockerfile_scripts/install_deepspeed.sh
+++ dockerfile_scripts/install_deepspeed.sh
@@ -4,8 +4,9 @@ set -e
 
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y pdsh libaio-dev
-# Triton is needed to build deepspeed's sparse_attn operation.
-python -m pip install triton
+# Triton==1.0.0 is needed to build deepspeed's sparse_attn operation.
+# It also version pins deepspeed
+python -m pip install triton==1.0.0
 python -m pip install pydantic==1.10.11
 DS_BUILD_OPS=1 python -m pip install $DEEPSPEED_PIP --no-binary deepspeed
 python -m deepspeed.env_report
