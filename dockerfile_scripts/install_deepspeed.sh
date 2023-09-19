06e6ec3ed0eda739750b927c3b5f86962230aab1
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-09-19 16:28:06 -0700

    remove compiling every deepspeed sublibrary

--- dockerfile_scripts/install_deepspeed.sh
+++ dockerfile_scripts/install_deepspeed.sh
@@ -6,7 +6,5 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y pdsh libaio-dev
 # Triton==1.0.0 is needed to build deepspeed's sparse_attn operation.
 # It also version pins deepspeed
-python -m pip install triton==1.0.0
-python -m pip install pydantic==1.10.11
-DS_BUILD_OPS=1 python -m pip install $DEEPSPEED_PIP --no-binary deepspeed
+python -m pip install $DEEPSPEED_PIP --no-binary deepspeed
 python -m deepspeed.env_report
