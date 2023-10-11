fa5426256533d7323d0b65d5d43414c15bf656fd
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-10-11 16:31:54 -0700

    deepspeed build args not getting used for some reason

--- dockerfile_scripts/install_deepspeed.sh
+++ dockerfile_scripts/install_deepspeed.sh
@@ -5,7 +5,8 @@ set -e
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y pdsh libaio-dev
 #Precompile deepspeed ops except sparse_attn which has dubious support
-DS_BUILD_OPS=1 DS_BUILD_SPARSE_ATTN=0
+export DS_BUILD_OPS=1
+export DS_BUILD_SPARSE_ATTN=0
 #Remove 5.2 from TORCH_CUDA_ARCH_LIST, it is no longer supported by deepspeed
 export TORCH_CUDA_ARCH_LIST=|echo $TORCH_CUDA_ARCH_LIST|sed 's/5.2 //'
 python -m pip install $DEEPSPEED_PIP --no-binary deepspeed
