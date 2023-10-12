9f5b9f41797c47704d588cb8cccad8e84f401f99
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-10-12 10:29:32 -0700

    bad bash syntax

--- dockerfile_scripts/install_deepspeed.sh
+++ dockerfile_scripts/install_deepspeed.sh
@@ -8,6 +8,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y pdsh libaio-dev
 export DS_BUILD_OPS=1
 export DS_BUILD_SPARSE_ATTN=0
 #Remove 5.2 from TORCH_CUDA_ARCH_LIST, it is no longer supported by deepspeed
-export TORCH_CUDA_ARCH_LIST=|echo $TORCH_CUDA_ARCH_LIST|sed 's/5.2 //'
+export TORCH_CUDA_ARCH_LIST=`echo $TORCH_CUDA_ARCH_LIST|sed 's/5.2 //'`
 python -m pip install $DEEPSPEED_PIP --no-binary deepspeed
 python -m deepspeed.env_report
