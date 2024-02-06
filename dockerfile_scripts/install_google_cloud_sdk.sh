ec21921e8f7e60bf44678f024d58fcc1f5dad4c4
Mikhail Kardash <mikhail.kardash@hpe.com>
2024-02-06 13:27:48 -0800

    cloud cli?

--- dockerfile_scripts/install_google_cloud_sdk.sh
+++ dockerfile_scripts/install_google_cloud_sdk.sh
@@ -5,4 +5,4 @@ set -e
 apt-get update -y && echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 DEBIAN_FRONTEND=noninteractive apt-get install apt-transport-https ca-certificates gnupg -y
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-apt-get update -y && apt-get install google-cloud-cli google-cloud-sdk -y
+apt-get update -y && apt-get install google-cloud-cli -y
