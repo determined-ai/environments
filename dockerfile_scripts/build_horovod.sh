7b0d7627aa4a0276b991392c938508b632275fa4
crickett-hpe <chris.rickett@hpe.com>
2024-08-06 13:17:44 -0400

    Enable C++-17 version of Horovod to HPC PyTorch image

---
+++ dockerfile_scripts/build_horovod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Try and build a version of Horovod that works with c++-17, which is
+# required by the latest PyTorch
+export CUDA_HOME=/usr/local/cuda-12
+export HOROVOD_WITHOUT_GLOO=1
+export HOROVOD_CUDA_HOME=/usr/local/cuda
+export HOROVOD_NCCL_LINK=SHARED
+export HOROVOD_GPU_OPERATIONS=NCCL
+export HOROVOD_WITH_MPI=1
+export HOROVOD_WITH_PYTORCH=1
+export HOROVOD_WITHOUT_TENSORFLOW=1
+export HOROVOD_WITHOUT_MXNET=1
+pip install --no-cache-dir git+https://github.com/thomas-bouvier/horovod.git@compile-cpp17
+
