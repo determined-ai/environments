31918f9dc23906ef3f577536887c107de6637b36
crickett-hpe <chris.rickett@hpe.com>
2024-08-14 16:06:46 -0400

    Set aws plugin version to 1.9.2 after testing

--- dockerfile_scripts/build_aws.sh
+++ dockerfile_scripts/build_aws.sh
@@ -19,10 +19,8 @@ if [ "$OFI" = "1" ]; then
   # Install AWS_OFI_NCCL
 #  AWS_VER=v1.6.0
 #  AWS_VER_NUM=1.6.0
-#  AWS_VER=v1.9.2
-#  AWS_VER_NUM=1.9.2
-  AWS_VER=v1.10.0
-  AWS_VER_NUM=1.10.0
+  AWS_VER=v1.9.2
+  AWS_VER_NUM=1.9.2
   AWS_NAME=aws-ofi-nccl
   AWS_FILE="${AWS_NAME}-${AWS_VER_NUM}"
   # cuda install dir likely dependent on BaseOS (i.e. ubuntu 20.02)
